2fe7dc819b893117300b574597da663882778c3e
Nethaji2606 <ntofcl2699@gmail.com>
2026-04-15 14:02:00 +0530

    Create data.py

---
+++ src/bike_tool/data.py
@@ -0,0 +1,59 @@
+# bike_tool/data.py
+# Centralized bike data. Add or edit entries here.
+
+BIKES = {
+    "Royal Enfield": {
+        "Classic 350": {
+            "Ex-Showroom Price": 193000,
+            "Engine Displacement cc": 349,
+            "Max Power bhp": 20.2,
+            "Max Torque Nm": 27,
+            "Fuel Tank Capacity L": 13,
+            "Mileage kmpl": 35
+        },
+        "Meteor 350": {
+            "Ex-Showroom Price": 200000,
+            "Engine Displacement cc": 349,
+            "Max Power bhp": 20.2,
+            "Max Torque Nm": 27,
+            "Fuel Tank Capacity L": 15,
+            "Mileage kmpl": 37
+        }
+    },
+    "Yamaha": {
+        "R15 V4": {
+            "Ex-Showroom Price": 182000,
+            "Engine Displacement cc": 155,
+            "Max Power bhp": 18.4,
+            "Max Torque Nm": 14.1,
+            "Fuel Tank Capacity L": 11,
+            "Mileage kmpl": 45
+        },
+        "FZ 25": {
+            "Ex-Showroom Price": 160000,
+            "Engine Displacement cc": 249,
+            "Max Power bhp": 20.8,
+            "Max Torque Nm": 20,
+            "Fuel Tank Capacity L": 14,
+            "Mileage kmpl": 40
+        }
+    },
+    "KTM": {
+        "Duke 390": {
+            "Ex-Showroom Price": 311000,
+            "Engine Displacement cc": 399,
+            "Max Power bhp": 43.5,
+            "Max Torque Nm": 37,
+            "Fuel Tank Capacity L": 13.4,
+            "Mileage kmpl": 28
+        },
+        "RC 200": {
+            "Ex-Showroom Price": 250000,
+            "Engine Displacement cc": 199,
+            "Max Power bhp": 25,
+            "Max Torque Nm": 19.2,
+            "Fuel Tank Capacity L": 10,
+            "Mileage kmpl": 35
+        }
+    }
+}
